48f6df155450c538d94c0033edc5dfa188b97c8c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-07-13 08:54:16 +0700

    change config.py

--- classification/config.py
+++ classification/config.py
@@ -44,7 +44,16 @@ finetune_epoch = {'SetFit/sst2': 3, "fancyzhx/ag_news": 2, "fancyzhx/yelp_polari
 finetune_mlp_epoch = {'SetFit/sst2': 30, "fancyzhx/ag_news": 5, "fancyzhx/yelp_polarity": 3, "fancyzhx/dbpedia_14": 3}
 
 # Config for CBM training
-concept_set = {'SetFit/sst2': concepts.sst2, "fancyzhx/yelp_polarity": concepts.yelpp, "fancyzhx/ag_news": concepts.agnews, "fancyzhx/dbpedia_14": concepts.dbpedia, "Duyacquy/Single-label-medical-abstract": concepts.med_abs, "Duyacquy/Legal-text": concepts.legal, "Duyacquy/Ecommerce-text": concepts.ecom, "Duyacquy/Stack-overflow-question": concepts.stackoverflow}
+concept_set = {'SetFit/sst2': concepts.sst2, 
+               "fancyzhx/yelp_polarity": concepts.yelpp, 
+               "fancyzhx/ag_news": concepts.agnews, 
+               "fancyzhx/dbpedia_14": concepts.dbpedia, 
+               "Duyacquy/Single-label-medical-abstract": concepts.med_abs, 
+               "Duyacquy/Legal-text": concepts.legal, 
+               "Duyacquy/Ecommerce-text": concepts.ecom, 
+               "Duyacquy/Stack-overflow-question": concepts.stackoverflow,
+               "Duyacquy/Pubmed-20k": concepts.pubmed,
+               "Duyacquy/UCI-drug": concepts.drug} # Done
 
 cbl_epochs = {'SetFit/sst2': 10, "fancyzhx/ag_news": 10,
               "fancyzhx/yelp_polarity": 10, "fancyzhx/dbpedia_14": 10,
